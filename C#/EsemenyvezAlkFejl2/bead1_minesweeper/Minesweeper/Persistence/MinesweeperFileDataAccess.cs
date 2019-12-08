2c6a22fd7079f3cd440301caffec5ba83bf0c4e1
Irodaimunkas <gonda.david18@gmail.com>
2019-12-08 03:31:50 +0100

    deleted comments in FileDataAccess

--- C#/EsemenyvezAlkFejl2/bead1_minesweeper/Minesweeper/Persistence/MinesweeperFileDataAccess.cs
+++ C#/EsemenyvezAlkFejl2/bead1_minesweeper/Minesweeper/Persistence/MinesweeperFileDataAccess.cs
@@ -9,33 +9,7 @@ using Minesweeper.Model;
 namespace Minesweeper.Persistence
 {
     public class MinesweeperFileDataAccess : IMinesweeperDataAccess
-    {/*
-        public async Task LoadAsync(String path, Player player, Field[,] map, Int32 size)
-        {
-            using (StreamReader reader = new StreamReader(path)) // fájl megnyitása
-            {
-                String line = await reader.ReadLineAsync();
-                size = Int32.Parse(line);
-                line = await reader.ReadLineAsync();
-                Enum.TryParse(line, out player);
-                map = new Field[size, size];
-                Console.WriteLine(size);
-                for (Int32 i = 0; i < size; i++)
-                {
-                    for (Int32 j = 0; j < size; j++)
-                    {
-                        line = await reader.ReadLineAsync();
-                        String[] split;
-                        split = line.Split(' ');
-                        map[i, j].value = Int32.Parse(split[0]);
-                        Enum.TryParse(split[1], out map[i, j].fieldType);
-                        map[i, j].visible = Boolean.Parse(split[2]);
-                    }
-                }
-            }
-
-        }
-        */
+    {
         public async Task<MinesweeperTable> LoadAsync(string path)
         {
             using (StreamReader reader = new StreamReader(path)) // fájl megnyitása
@@ -61,23 +35,7 @@ namespace Minesweeper.Persistence
                 return table;
             }
         }
-        /*
-        public async Task SaveAsync(String path, Player player, Field[,] map, Int32 size)
-        {
-            using (StreamWriter writer = new StreamWriter(path)) // fájl megnyitása
-            {
-                await writer.WriteLineAsync(size.ToString());
-                await writer.WriteLineAsync(player.ToString());
-                for(Int32 i = 0; i < size; i++)
-                {
-                    for(Int32 j = 0; j < size; j++)
-                    {
-                        await writer.WriteLineAsync(map[i, j].value.ToString() + " " + map[i, j].fieldType.ToString() + " " + map[i, j].visible.ToString());
-                    }
-                }
-            }
-        }
-        */
+
         public async Task SaveAsync(string path, MinesweeperTable gameTable)
         {
             using (StreamWriter writer = new StreamWriter(path)) // fájl megnyitása
