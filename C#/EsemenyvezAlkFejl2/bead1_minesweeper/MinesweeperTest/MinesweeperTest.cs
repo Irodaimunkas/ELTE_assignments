e640b8279ce42bac92262d04443523b4297a90b7
Irodaimunkas <gonda.david18@gmail.com>
2019-12-08 03:53:06 +0100

    added test (not done yet)

---
+++ C#/EsemenyvezAlkFejl2/bead1_minesweeper/MinesweeperTest/MinesweeperTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper.Model;
+using Minesweeper.Persistence;
+
+namespace MinesweeperTest
+{
+    [TestClass]
+    public class MinesweeperTest
+    {
+        private MinesweeperModel _model;
+        private IMinesweeperDataAccess _dataAcces;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+        }
+    }
+}
---
+++ C#/EsemenyvezAlkFejl2/bead1_minesweeper/MinesweeperTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MinesweeperTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MinesweeperTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a6b19f8-45e0-468c-aa9c-4d53fcc58514")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
