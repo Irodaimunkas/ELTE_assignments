d792439061269b13d8ffdd9c8a2ce275ff58b5cc
Irodaimunkas <gonda.david18@gmail.com>
2019-12-08 04:31:19 +0100

    deleted async load/save tests

--- C#/EsemenyvezAlkFejl2/bead1_minesweeper/MinesweeperTest/MinesweeperTest.cs
+++ C#/EsemenyvezAlkFejl2/bead1_minesweeper/MinesweeperTest/MinesweeperTest.cs
@@ -20,6 +20,9 @@ namespace MinesweeperTest
             _mock.Setup(_mock => _mock.LoadAsync(It.IsAny<string>())).Returns(
                 Task.FromResult(new MinesweeperTable(6))
             );
+            _mock.Setup(_mock => _mock.SaveAsync(It.IsAny<String>(), new MinesweeperTable(6)));
+
+            _model = new MinesweeperModel(_mock.Object);
         }
     }
 }
