c8b4173daeb170e510dbb1047fa64fbbe3ce2cfb
Irodaimunkas <gonda.david18@gmail.com>
2019-12-13 00:28:18 +0100

    changes in waiter

--- ADA/ada_bead2/main-waiter.adb
+++ ADA/ada_bead2/main-waiter.adb
@@ -2,19 +2,21 @@ separate(main)
 task body Waiter is
     Order_Time : Duration;
     Serve_Time : Duration;
-    Plate_To_Serve : Plate_Access;
+    Plate_To_Serve_Acc : Plate_Access;
+    Plate_To_Serve_Int :  Integer;
 begin
     loop
         select
-            accept Order(Food : in Pizza_Types; Table : in Integer; Dur : out Duration);
-            Safe_Random.Get_Duration(Order_Time);
-            delay Order_Time;
-            Safe_Random.Get_Duration(Dur);
-            Safe_Random.Get_Duration(Serve_Time);
-            delay Serve_Time;
-            Ada.Text_IO.Put_Line(Serve_Time'Image);
-            Plate_To_Serve := new Plate(Table);
-            Pizzeria.Waiter_Ready(ID);
+            accept Order(Food : in Pizza_Types; Table : in Integer; Dur : out Duration) do
+                Plate_To_Serve_Int := Table;
+                Safe_Random.Get_Duration(Order_Time);
+                delay Order_Time;
+                Safe_Random.Get_Duration(Dur);
+            end Order;
         end select;
+        Safe_Random.Get_Duration(Serve_Time);
+        delay Serve_Time;
+        Plate_To_Serve_Acc := new Plate(Plate_To_Serve_Int);
+        Pizzeria.Waiter_Ready(ID);
     end loop;
 end Waiter;
